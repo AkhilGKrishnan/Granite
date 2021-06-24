1eefbde979d303775a987d2ee78702238981d976
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-06-24 09:10:32 +0530

    Set up react environment

---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    /*eslint no-undef: "off"*/
+    initializeLogger();
+    setAuthHeaders(setLoading);
+    // logger.info("Never use console.log");
+    // logger.error("Never use console.error");
+  }, []);
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/about" render={() => <div>About</div>} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
