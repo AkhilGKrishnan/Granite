38ce31aa0bbdaa56a669f4296082068189055295
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-06-24 20:58:27 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import CreateTask from "components/Tasks/CreateTask";
 import Dashboard from "components/Dashboard";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -8,6 +11,7 @@ const App = () => {
   useEffect(() => {
     /*eslint no-undef: "off"*/
     initializeLogger();
+    registerIntercepts();
     setAuthHeaders(setLoading);
     // logger.info("Never use console.log");
     // logger.error("Never use console.error");
@@ -15,9 +19,9 @@ const App = () => {
 
   return (
     <Router>
+      <ToastContainer />
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
--- db/migrate/20210624144124_add_slug_to_task.rb
+++ db/migrate/20210624144124_add_slug_to_task.rb
@@ -1,6 +1,6 @@
 class AddSlugToTask < ActiveRecord::Migration[6.1]
   def change
-    add_column :tasks, :slug, :string
+    add_column :tasks, :slug, :string, null: false
     add_index :tasks, :slug, unique: true
   end
 end
