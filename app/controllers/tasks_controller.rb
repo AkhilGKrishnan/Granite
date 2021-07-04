6d7bd0825c2dce5416874bf34ca01deebba3731b
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-07-04 17:28:51 +0530

    Added login feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user_using_x_auth_token, except: %i[new edit]
   before_action :load_task, only: %i[show update destroy]
   def index
     tasks = Task.all
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import CreateTask from "components/Tasks/CreateTask";
+import { either, isEmpty, isNil } from "ramda";
 import Dashboard from "components/Dashboard";
 import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import ShowTask from "components/Tasks/ShowTask";
 import EditTask from "components/Tasks/EditTask";
 import { initializeLogger } from "common/logger";
+import Login from "components/Authentication/Login";
 import Signup from "components/Authentication/Signup";
+import PrivateRoute from "components/Common/PrivateRoute";
+import PageLoader from "components/PageLoader";
+import { getFromLocalStorage } from "src/helpers/storage.js";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const authToken = getFromLocalStorage("authToken");
+  const isLoggedIn = !either(isNil, isEmpty)(authToken) && authToken !== "null";
 
   useEffect(() => {
     /*eslint no-undef: "off"*/
@@ -21,6 +28,14 @@ const App = () => {
     // logger.error("Never use console.error");
   }, []);
 
+  if (loading) {
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <ToastContainer />
@@ -30,6 +45,13 @@ const App = () => {
         <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute
+          path="/"
+          redirectRoute="/login"
+          condition={isLoggedIn}
+          component={Dashboard}
+        />
       </Switch>
     </Router>
   );
