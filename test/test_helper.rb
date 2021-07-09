d10ddf263385e8e3e13ad43d5c59674e44d61cbd
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-07-09 23:50:44 +0530

    Added controller tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,21 @@
-# frozen_string_literal: true
-
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+class ActiveSupport::TestCase
+  include ActionView::Helpers::TranslationHelper
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # fixtures :all
 
-    # Add more helper methods to be used by all tests here...
-  end
+  # Add more helper methods to be used by all tests here...
+end
+
+def headers(user, options = {})
+  {
+    'X-Auth-Token' => user.authentication_token,
+    'X-Auth-Email' => user.email
+  }.merge(options)
 end
